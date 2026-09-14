604237f5e1d63fb8ebd15e96ca6a4cf3937a2cf2
JDave116 <simonye2022@outlook.com>
2026-09-14 13:19:30 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,15 @@
+public class Date{
+	public static void main(String[] args){
+		
+		String day;
+		day = ("Thursday");
+		int date = 18;
+		String month;
+		month = ("July "); 
+		int year = 2019;
+		
+		System.out.println(day+", "+month+date+", "+year);
+		System.out.println(day+" "+date+" "+month+year);
+		
+	}
+}
