edd60b930264a4ccd63de16692615701262cd65a
JDave116 <simonye2022@outlook.com>
2026-09-17 12:32:45 -0700

    Add files via upload

--- ConvertTime.java
+++ ConvertTime.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 		public static void main (String[] args){
 			System.out.println("Enter Seconds");
 			Scanner in = new Scanner(System.in);
-			int Sec = in.nextInt();
-			int Hour = Sec % 3600;
-			int Min = (Sec % 3600)/60;
-			int Seconds = Sec/60;
 			
-			System.out.printf("%d Seconds = %d Hours, %d Minutes, %d Seconds", Sec, Hour, Min, Seconds);
+			int sec = in.nextInt();
+			
+			int hour = sec/3600;
+			int min = (sec%3600)/60;
+			int seconds = sec % 60;
+			
+			System.out.printf("%d Seconds = %d Hours, %d Minutes, %d Seconds%n", sec, hour, min, seconds);
 	}
 }
