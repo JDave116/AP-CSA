1a6de86728163d7d8830f19c1ba77496348003a7
JDave116 <simonye2022@outlook.com>
2026-09-22 13:17:04 -0700

    Add files via upload

---
+++ Multadd.java
@@ -0,0 +1,9 @@
+public class Multadd{
+	public static void multadd(double a, double b, double c){
+		System.out.println ( a*b+c );
+	}
+	
+	public static void main(String[] args){
+		multadd(1,2,3);
+	}
+}
